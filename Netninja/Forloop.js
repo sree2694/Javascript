a4e5c558b21e8046268218cf9d458577244060ec
sree2694 <58017164+sree2694@users.noreply.github.com>
2021-08-06 15:24:07 +0530

    Loops

---
+++ Netninja/Forloop.js
@@ -0,0 +1,9 @@
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i <= links.length; i++) {
+
+    console.log("This is Link number" + i);
+
+}
+
+document.write(" All links now looped");
